d5399a703ead1ec44f146f29d94f4fc7c7c50b66
haenno <haenno@web.de>
2021-11-15 21:49:47 +0100

    Chatbot commit #3

--- rebuild-server.sh
+++ rebuild-server.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # stops docker-compose, reloads git repo, builds docker-compose fresh and restarts
+git fetch origin
+git reset --hard origin/main
 git -C /home/rjhadmin/tstsrv/ fetch
 git -C /home/rjhadmin/tstsrv/ pull
 docker-compose --project-directory /home/rjhadmin/tstsrv/ down
-docker-compose --project-directory /home/rjhadmin/tstsrv/ build
+# delete also all images
+#docker rmi $(docker images -q) 
+# reset even more docker files
+#docker system prune -a --volumes
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker-compose --project-directory /home/rjhadmin/tstsrv/ build --no-cache
 docker-compose --project-directory /home/rjhadmin/tstsrv/ up -d
 
 
