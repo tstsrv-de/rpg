681fa314dda090a58632e7e1240bc48bdbc3fcd1
Henning 'haenno' Beier <haenno@web.de>
2021-11-24 17:13:28 +0100

    add user registration #19

--- rpg/rjh_rpg/views.py
+++ rpg/rjh_rpg/views.py
@@ -11,36 +11,30 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 def signup(request):
-    if request.user.is_authenticated() == True:
-        return redirect('home')
-    else:
-        if request.method == "POST":
-            if request.POST['password1'] == request.POST['password2']:
-                try:
-                    User.objects.get(username = request.POST['username'])
-                    return render (request,'signup.html', {'error':'Benutzername ist bereits vergeben!'})
-                except User.DoesNotExist:
-                    user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
-                    auth.login(request,user)
-                    return redirect('home')
-            else:
-                return render (request,'signup.html', {'error':'Passwörter stimmen nicht überein!'})
-        else:
-            return render(request,'signup.html')
-
-def login(request):
-    if request.user.is_authenticated() == True:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
-            if user is not None:
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                User.objects.get(username = request.POST['username'])
+                return render (request,'signup.html', {'error':'Benutzername ist bereits vergeben!'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
                 auth.login(request,user)
                 return redirect('home')
-            else:
-                return render (request,'login.html', {'error':'Benutzername oder Passwort falsch!'})
         else:
-            return render(request,'login.html')
+            return render (request,'signup.html', {'error':'Passwörter stimmen nicht überein!'})
+    else:
+        return render(request,'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render (request,'login.html', {'error':'Benutzername oder Passwort falsch!'})
+    else:
+        return render(request,'login.html')
 
 def logout(request):
     auth.logout(request)
