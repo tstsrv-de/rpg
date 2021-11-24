40ed6822e8f9c001c47ce695f13ff069dda42ca7
haenno <haenno@web.de>
2021-11-24 12:27:17 +0100

    Update cron script again

--- rpg-update-cron.sh
+++ rpg-update-cron.sh
@@ -5,13 +5,13 @@ git -C /home/rjhadmin/tstsrv/ fetch origin
 if  [ `git -C /home/rjhadmin/tstsrv/ rev-list HEAD...origin/main --count` != 0 ] 
 then
     echo ' --> RPG update > Remote git repo newer > Updating...'
-    docker-compose stop rpg
+    docker-compose --project-directory /home/rjhadmin/tstsrv/ stop rpg
     git -C /home/rjhadmin/tstsrv/ reset --hard origin/main
     git -C /home/rjhadmin/tstsrv/ fetch
     git -C /home/rjhadmin/tstsrv/ pull
-    docker-compose run rpg python rpg/manage.py makemigrations
-    docker-compose run rpg python rpg/manage.py migrate
-    docker-compose start rpg
+    docker-compose --project-directory /home/rjhadmin/tstsrv/ run rpg python rpg/manage.py makemigrations
+    docker-compose --project-directory /home/rjhadmin/tstsrv/ run rpg python rpg/manage.py migrate
+    docker-compose --project-directory /home/rjhadmin/tstsrv/ start rpg
     echo ' --> RPG update > Update done!'
 else
     echo ' --> RPG update > Remote git repo same as local > Nothing to do!'
