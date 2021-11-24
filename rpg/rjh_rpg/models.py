9f6614a3187f98e8af8ee7ce8df296341d3f8307
Henning 'haenno' Beier <haenno@web.de>
2021-11-24 20:21:19 +0100

    add user registration #37

--- rpg/rjh_rpg/models.py
+++ rpg/rjh_rpg/models.py
@@ -6,6 +6,10 @@ class User(models.Model):
     nickname = models.CharField(unique=True,max_length=100)
     email = models.EmailField(unique=True)
     text = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.nickname
+    
     
 class UserChar(models.Model):
     user = models.ForeignKey(User, on_delete=models.RESTRICT)
