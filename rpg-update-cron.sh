819e7982360dac7750984fb4d07aa166a0dff4dd
haenno <haenno@web.de>
2021-11-24 13:04:00 +0100

    Cronscript add times and date

--- rpg-update-cron.sh
+++ rpg-update-cron.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # start after update on rpg part of repo
-echo ' --> RPG update > Starting...'
+now=$(date)
+echo '$now --> RPG update > Starting...'
 git -C /home/rjhadmin/tstsrv/ fetch origin
 if  [ `git -C /home/rjhadmin/tstsrv/ rev-list HEAD...origin/main --count` != 0 ] 
 then
-    echo ' --> RPG update > Remote git repo newer > Updating...'
+    echo '$now --> RPG update > Remote git repo newer > Updating...'
     docker-compose --project-directory /home/rjhadmin/tstsrv/ stop rpg
     git -C /home/rjhadmin/tstsrv/ reset --hard origin/main
     git -C /home/rjhadmin/tstsrv/ fetch
@@ -12,8 +13,8 @@ then
     docker-compose --project-directory /home/rjhadmin/tstsrv/ run rpg python rpg/manage.py makemigrations
     docker-compose --project-directory /home/rjhadmin/tstsrv/ run rpg python rpg/manage.py migrate
     docker-compose --project-directory /home/rjhadmin/tstsrv/ start rpg
-    echo ' --> RPG update > Update done!'
+    echo '$now --> RPG update > Update done!'
 else
-    echo ' --> RPG update > Remote git repo same as local > Nothing to do!'
+    echo '$now --> RPG update > Remote git repo same as local > Nothing to do!'
 fi
-echo ' --> RPG update > Finished!'
+echo '$now --> RPG update > Finished!'
