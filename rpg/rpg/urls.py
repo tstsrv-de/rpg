2454f144277d79f020d833d1d53b45e45448d9c2
Henning 'haenno' Beier <haenno@web.de>
2021-11-24 15:33:03 +0100

    add user registration #3

--- rpg/rpg/urls.py
+++ rpg/rpg/urls.py
@@ -20,6 +20,6 @@ from rjh_rpg import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('rpg.urls')),
     path('admin/', admin.site.urls),
 ]
