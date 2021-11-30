e1aa0c617fc05e3a485ff097aa51a1cbf9424229
Henning 'haenno' Beier <haenno@web.de>
2021-11-30 21:22:35 +0100

    Remove old testing containers and data

--- rjh/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from myapp.models import Webpage, Topic, User, AccessRecord
-
-admin.site.register(Webpage)
-admin.site.register(Topic)
-admin.site.register(User)
-admin.site.register(AccessRecord)
