555713fcae002339cac56fa212ced70560d86e49
Henning 'haenno' Beier <haenno@web.de>
2021-12-01 23:38:17 +0100

    Add countdown in lobby

--- rpg/rjh_rpg/consumer_lobby.py
+++ rpg/rjh_rpg/consumer_lobby.py
@@ -1,4 +1,6 @@
+from datetime import time
 import json
+from json.encoder import JSONEncoder
 from channels.generic.websocket import AsyncWebsocketConsumer
 from rjh_rpg.models import HelperCounter
 from asgiref.sync import sync_to_async
@@ -7,6 +9,8 @@ from rjh_rpg.models import GameScenes, LobbySlots
 from django.contrib.auth.models import User
 from rjh_rpg.models import GameState
 from rjh_rpg.models import UserChar
+from django.db.models.functions import Now
+from datetime import datetime
 
 class Consumer(AsyncWebsocketConsumer):
     
@@ -29,6 +33,26 @@ class Consumer(AsyncWebsocketConsumer):
     html_table_bottom = """
     </tr>
     </table>
+    
+    """
+    
+    html_jump_to_game = """
+    
+    <form action="{% url 'scene_jumper' %}" method="POST" id="interest-select" class="form-inline">
+    <div class="form-group">
+    {% csrf_token %}
+
+    <select  class="custom-select form-control" size="5" id="scene_id" name="scene_id" >
+    {% for scene in game_scenes_list %} 
+        <option value={{scene.id}}>{{scene.name}} (Spieler im Chat: {{ scene.players_in_chat_counter }}, davon spielbereit: {{ scene.waiting_players }}/{{ scene.req_players }})</option>
+    {% endfor %}
+    </select> 
+    <input type="hidden" id="char_id" name="char_id" value="{{ char_id }}">
+    <input class="btn btn-primary" type="submit" value="&#10145; Lobby der Szene betreten!"/>
+    </div>
+    </form>
+
+    **Countdown**
     
     """
     
@@ -59,10 +83,17 @@ class Consumer(AsyncWebsocketConsumer):
         
     async def msg_group_send_content(self, event):
         
+        try:
+            countdown = event['countdown']
+        except:
+            countdown = ""
+            
+        print("countdown from send: " + str(countdown))
         
         scene_name = await self.db_get_scene_name()
         html = self.html_table_top.replace("**scene_name**",scene_name) 
         num_players = await self.db_get_num_players()
+        
         
         for slot_id in range(num_players):
             if slot_id != 0 and slot_id % 3 == 0: # linebreak in table every 4 slots
@@ -80,6 +111,8 @@ class Consumer(AsyncWebsocketConsumer):
                 char_name = await self.db_get_slot_char_name(slot_id)
                 slot_template = slot_template.replace('**js_button_function**','free_the_slot')
                 slot_template = slot_template.replace('**button_text**','Platz freigeben')
+                
+                # (TODO!) Disable buttons
                 # check if own entry or not, disable button if it is so
                 #if slot_char_id == self.char_id:
                 slot_template = slot_template.replace('**button_disabled**','')
@@ -99,6 +132,25 @@ class Consumer(AsyncWebsocketConsumer):
         html = html + self.html_table_bottom
         html = html.replace('\n','')
         
+        ## add countdown if it is set:
+        
+        print("countdown string sender:" + countdown)
+        if countdown == "":
+            countdown_html = '' # no countdown
+        else: # countdown is running / game is ready
+            countdown = int(countdown)
+            if countdown < 12: 
+                countdown = (countdown -12) * -1
+                countdown_html = """
+                <p style="color:green;">**seconds** Sekunden bis Spielstart!</p>
+                """
+                countdown_html = countdown_html.replace('**seconds**', str(countdown))
+            else:
+                countdown_html = """
+                <p style="color:red;">Go!</p>
+                """
+        html = html + countdown_html 
+
         await self.send(text_data=json.dumps({ # send data update
             'lobby_msg': str(html),
         }))
@@ -117,28 +169,75 @@ class Consumer(AsyncWebsocketConsumer):
 
         if message == 'heartbeat':
             # (TODO!) update timestamps, delete old entrys
-            pass
-
+            
+            # check timestamps, delete old and zombie  entrys
+           
+            # check if all slots are taken
+            req_players = await self.db_get_num_players()
+            players_in_lobby = 0
+            for slot in range(req_players):
+                slot_state = await self.db_get_slot_state(slot)
+                if slot_state == 1: # slot is used
+                    players_in_lobby = players_in_lobby + 1
+                    
+            if req_players == players_in_lobby:
+                print("lobby voll:" + str(players_in_lobby) + "/" + str(req_players))
+                
+                # create/check countdown
+                # do all slots have the same timestamp in countdown?
+                
+                locked_in_datetimes = await self.db_get_list_datetime_locked_in(self.scene_id)
+                last_timestamp = locked_in_datetimes[0]
+                
+                # singleplayer check and repair
+                if last_timestamp is None and req_players == 1: 
+                    last_timestamp = datetime.now()
+                    
+                timestamps_are_the_same = True
+                
+                for timestamp in locked_in_datetimes:
+                    if last_timestamp != timestamp:
+                        timestamps_are_the_same = False
+                
+                if timestamps_are_the_same:  # check if countdown is over
+                    now = int(datetime.now().strftime('%s'))
+                    
+                    last_timestamp = int(last_timestamp.strftime('%s'))
+                    timediff = now-last_timestamp
+                    
+                    if timediff > 1: # wait a second for updates...
+                        await self.channel_layer.group_send(
+                            self.msg_group_name, { 
+                                                  'type': 'msg_group_send_content',  
+                                                  'countdown' : str(timediff), 
+                                                  } 
+                        )            
+                    
+                else:
+                    set_datetimes = await self.db_set_datetime_locked_in()
+                
+            else:
+                print("lobby nicht voll:" + str(players_in_lobby) + "/" + str(req_players))
+                
+                
+            
+            # if yes, then replace slot table with form to join a scene, broadcast to all
+            
         if message == 'free_the_slot':
-
-            #text_data_json = json.loads(text_data)
             char_id = text_data_json['char_id']
             slot_id = text_data_json['slot_id']
             
             set_new_char_to_slot = await self.db_free_slot(slot_id, char_id)
-            print(set_new_char_to_slot)
             
             await self.channel_layer.group_send(
                 self.msg_group_name, { 'type': 'msg_group_send_content', 'char_id' : char_id,  } 
             )            
         
         if message == 'take_the_slot':
-            #text_data_json = json.loads(text_data)
             char_id = text_data_json['char_id']
             slot_id = text_data_json['slot_id']
             
             set_new_char_to_slot = await self.db_set_char_to_slot(slot_id, char_id)
-            print(set_new_char_to_slot)
                      
             await self.channel_layer.group_send(
                 self.msg_group_name, { 'type': 'msg_group_send_content',  'char_id' : char_id, } 
@@ -233,5 +332,45 @@ class Consumer(AsyncWebsocketConsumer):
                 return "delete: ok"
         except:
             return "delele: wierd error"
+
+    @database_sync_to_async     
+    def db_get_list_datetimes(self, slot_id):
+        self.scene_id = self.scope['url_route']['kwargs']['scene_id']
+        
+        list_of_slot_entrys = LobbySlots.objects.filter(game_scene_id=self.scene_id).order_by('slot_id')
+        
+        locked_in = []
+        slot_taken = []
+        
+        for row in list_of_slot_entrys:
+            locked_in.append(row.datetime_locked_in)
+            slot_taken.append(row.datetime_slot_taken)
+            
+        return (locked_in,slot_taken)
+
+
+    @database_sync_to_async     
+    def db_get_list_datetime_locked_in(self, slot_id):
+        self.scene_id = self.scope['url_route']['kwargs']['scene_id']
+        list_of_slot_entrys = LobbySlots.objects.filter(game_scene_id=self.scene_id).order_by('slot_id')
+        locked_in = []
+        
+        for row in list_of_slot_entrys:
+            locked_in.append(row.datetime_locked_in)
+            
+        return locked_in
+    
+    @database_sync_to_async     
+    def db_set_datetime_locked_in(self):
+        self.scene_id = self.scope['url_route']['kwargs']['scene_id']
+        try:
+            LobbySlots.objects.filter(game_scene_id=self.scene_id).update(datetime_locked_in=Now())
+            return True
+        except:
+            return False
+        
+
+
+    
 
     pass
