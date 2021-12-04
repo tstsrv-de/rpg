eea8f582a0a9d8166dae5193701f5ccd75b49d4f
haenno <haenno@web.de>
2021-12-04 23:56:51 +0100

    Pimp chat

--- rpg/rjh_rpg/consumers.py
+++ rpg/rjh_rpg/consumers.py
@@ -17,21 +17,6 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'welcome_message',
-            }
-        )
-
-    async def welcome_message(self, event):
-        chars_in_chat = await self.db_get_list_chars_in_chat()
-
-        await self.send(text_data=json.dumps({
-            'username': 'Aktuell im Chat',
-            'message': chars_in_chat,
-        }))
-
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -42,17 +27,45 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        username = text_data_json['username']
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chatroom_message',
-                'message': message,
-                'username': username,
-            }
-        )
+        try:
+            message = text_data_json['message']
+        except:
+            message = ""
+
+        try:
+            ul = text_data_json['ul']
+        except:
+            ul = ""
+        
+        if ul == 'heatbeat':
+            chars_in_chat = await self.db_get_list_chars_in_chat()            
+            
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'ul_message',
+                    'ul': chars_in_chat,
+                }
+            )
+
+        if message != "":
+            username = text_data_json['username']
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chatroom_message',
+                    'message': message,
+                    'username': username,
+                }
+            )
+        
+    async def ul_message(self, event):
+        ul = event['ul']
+
+        await self.send(text_data=json.dumps({
+            'ul': ul,
+        }))            
 
     async def chatroom_message(self, event):
         message = event['message']
@@ -66,10 +79,10 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async     
     def db_get_list_chars_in_chat(self):
         list_of_chars_in_chat = GameState.objects.filter(place=self.room_name).order_by('char')
-        char_names = []
+        char_names = ""
         
         for row in list_of_chars_in_chat:
-            char_names.append(str(row.char))
+            char_names = char_names + str(row.char) + "<br />"
             
         return char_names
 
