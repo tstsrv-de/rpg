3b7b2771425972d41640bbf10575417628ac044a
haenno <haenno@web.de>
2021-12-27 19:51:41 +0100

    minor update helper script

--- local-win-dev-start.bat
+++ local-win-dev-start.bat
@@ -1,3 +1,6 @@
+@echo off
+echo Docker gestartet? > Weiter mit Tastendruck...
+pause >nul
 docker-compose -f local-win-docker-compose.yml build
 docker-compose -f local-win-docker-compose.yml up -d
 docker-compose -f local-win-docker-compose.yml exec rpg python rpg/manage.py makemigrations
