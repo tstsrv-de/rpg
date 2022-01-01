8b14a00e2bd9a53ee279d5475c24f546e4cc10ba
haenno <haenno@web.de>
2022-01-01 22:08:41 +0100

    Minor DB model change

--- rpg/rjh_rpg/models.py
+++ rpg/rjh_rpg/models.py
@@ -54,7 +54,7 @@ class UserChar(models.Model):
 
 class GameScenes(models.Model): # blueprint of the games
     name = models.CharField(unique=True, max_length=300)
-    req_players = models.BigIntegerField(default=1) # anzahl notweniger spieler
+    req_players = models.BigIntegerField(default=1) # number of required players
     welcome_text = models.TextField(default="Eine Gefahr zeigt sich im dunkeln...<br />Es raschelt...<br />", null=True, blank=True)
     enemy_name = models.CharField(default="Der Gegener", null=True, blank=True, max_length=300)
     enemy_hp = models.IntegerField(default=100) 
@@ -66,7 +66,7 @@ class GameScenes(models.Model): # blueprint of the games
 
 
 class LobbySlots(models.Model):
-    user_char_id = models.ForeignKey(UserChar, on_delete=models.CASCADE, unique=True)
+    user_char_id = models.OneToOneField(UserChar, on_delete=models.CASCADE, unique=True)
     game_scene_id = models.ForeignKey(GameScenes, on_delete=models.CASCADE) # also known as scene_id
     slot_id = models.IntegerField(null=True, blank=True) # also known as scene_id    (TODO!) install as a required field w/o null and blank   
     datetime_slot_taken = models.DateTimeField(default=datetime.now) 
@@ -76,10 +76,10 @@ class LobbySlots(models.Model):
  # better name would have been 'UserCharState'
  # since there is no information about the games - only of the user and its char
 class GameState(models.Model): 
-    char = models.ForeignKey(UserChar, on_delete=models.CASCADE, unique=True)
+    char = models.OneToOneField(UserChar, on_delete=models.CASCADE, unique=True)
     place = models.IntegerField(default=0) # 0 = weltmap, n > 0 = laufende_game_id
     charLogin = models.DateTimeField(default=datetime.now) 
-    char_user = models.ForeignKey(settings.AUTH_USER_MODEL, unique=True, on_delete=models.CASCADE, null=True, blank=True)
+    char_user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True, on_delete=models.CASCADE, null=True, blank=True)
     
     def __str__(self):
         return str(self.char)
