1bfa99e89ddf2dbe817bdee2c870d1ddbe4f23c1
haenno <haenno@web.de>
2022-01-01 22:42:18 +0100

    Add sample data for games

--- local-linux-dev-start.sh
+++ local-linux-dev-start.sh
@@ -2,5 +2,6 @@
 docker-compose -f local-win-docker-compose.yml up -d
 docker-compose -f local-win-docker-compose.yml exec rpg python rpg/manage.py makemigrations
 docker-compose -f local-win-docker-compose.yml exec rpg python rpg/manage.py migrate
+docker-compose -f local-win-docker-compose.yml exec rpg python rpg/manage.py loaddata db_sample_data.json
 docker-compose -f local-win-docker-compose.yml stop
 docker-compose -f local-win-docker-compose.yml up
--- local-win-dev-start.bat
+++ local-win-dev-start.bat
@@ -5,5 +5,6 @@ docker-compose -f local-win-docker-compose.yml build
 docker-compose -f local-win-docker-compose.yml up -d
 docker-compose -f local-win-docker-compose.yml exec rpg python rpg/manage.py makemigrations
 docker-compose -f local-win-docker-compose.yml exec rpg python rpg/manage.py migrate
+docker-compose -f local-win-docker-compose.yml exec rpg python rpg/manage.py loaddata db_sample_data.json
 docker-compose -f local-win-docker-compose.yml stop
 docker-compose -f local-win-docker-compose.yml up
