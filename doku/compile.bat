985a21cf522d758d3e6bdb63b896ce6dd867d1f5
derBart0815 <nur_fuer_spam1@gmx.de>
2022-01-20 16:14:55 +0100

    einleitung+Afgabenbeschr.

--- doku/compile.bat
+++ doku/compile.bat
@@ -2,5 +2,5 @@ lualatex thesis_main.tex
 biber thesis_main
 lualatex thesis_main.tex
 lualatex thesis_main.tex
-thesis_main.pdf
+
 del thesis_main.aux thesis_main.bbl thesis_main.bcf thesis_main.blg thesis_main.lof thesis_main.log thesis_main.lot thesis_main.out thesis_main.run.xml thesis_main.toc
